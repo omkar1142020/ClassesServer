35df4c4c3e865e61730e82dea26b9e52153d8be6
omkar1142020 <30465092+omkar1142020@users.noreply.github.com>
2019-11-18 09:24:27 +0530

    done

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const app= express();
 const database = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'test'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
